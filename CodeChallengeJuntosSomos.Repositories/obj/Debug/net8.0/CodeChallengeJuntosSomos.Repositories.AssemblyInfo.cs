28612b81592e992a99d45e16425b7b0199850db7
Raphael Faquim <efaquim@gmail.com>
2024-11-22 13:24:48 -0300

    Ajuste da classe Usecase

--- CodeChallengeJuntosSomos.Repositories/obj/Debug/net8.0/CodeChallengeJuntosSomos.Repositories.AssemblyInfo.cs
+++ CodeChallengeJuntosSomos.Repositories/obj/Debug/net8.0/CodeChallengeJuntosSomos.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeChallengeJuntosSomos.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b24cebafcf026dfd037488f97b2e88369e6ea77")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeChallengeJuntosSomos.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeChallengeJuntosSomos.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CodeChallengeJuntosSomos/obj/Debug/net8.0/CodeChallengeJuntosSomos.Api.AssemblyInfo.cs
+++ CodeChallengeJuntosSomos/obj/Debug/net8.0/CodeChallengeJuntosSomos.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeChallengeJuntosSomos.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b24cebafcf026dfd037488f97b2e88369e6ea77")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeChallengeJuntosSomos.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeChallengeJuntosSomos.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
